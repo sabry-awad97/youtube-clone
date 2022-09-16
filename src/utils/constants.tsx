491fd689ee3b32a9b16afa94e0b0f777298774fb
Sabry Awad <dr.sabry1997@gmail.com>
2022-09-16 15:39:30 +0200

    implement searchbar

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,16 +1,16 @@
-import MusicNoteIcon from '@mui/icons-material/MusicNote';
-import HomeIcon from '@mui/icons-material/Home';
-import CodeIcon from '@mui/icons-material/Code';
-import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
-import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
-import LiveTvIcon from '@mui/icons-material/LiveTv';
-import SchoolIcon from '@mui/icons-material/School';
-import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
 import CheckroomIcon from '@mui/icons-material/Checkroom';
-import GraphicEqIcon from '@mui/icons-material/GraphicEq';
-import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import CodeIcon from '@mui/icons-material/Code';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import HomeIcon from '@mui/icons-material/Home';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SchoolIcon from '@mui/icons-material/School';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
