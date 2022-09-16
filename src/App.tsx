1384d219232b66c038d134d32d062c603c112b08
Sabry Awad <dr.sabry1997@gmail.com>
2022-09-16 14:32:05 +0200

    implement routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import './App.css';
 
-const App = () => {
-  return <div>Hello</div>;
-};
+import { Box } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import {
+  ChannelDetail,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetail,
+} from './components';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
